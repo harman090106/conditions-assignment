3070c7c81a55f6014507c41e7d54114ea34e27dd
harman090106 <145844415+harman090106@users.noreply.github.com>
2024-02-22 23:19:12 +0530

    assignment

---
+++ conditions/q4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+    //q4.
+    int radius,height;
+    cout<<"Enter radius : "
+    cin>>radius;
+    cout<<"Enter height : ";
+    cin>>height;
+    float volume=3.14*radius*radius*height;
+    cout<<volume;
+}
---
+++ conditions/q5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //q5.
+    char ch1,ch2;
+    cout<<"Enter two charcters : ";
+    cin>>ch1>>ch2;
+    cout<<"Difference between ascii of characters "<<(int)ch1-(int)ch2;
+}
